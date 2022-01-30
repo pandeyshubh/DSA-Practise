9db3346e31ca12f1c8d5d6f277d67307cea8284b
pandeyshubh <shubhankarpandey21@gmail.com>
2022-01-30 19:48:11 +0530

    second method added for Missing Number problem

--- searching/MissingNumber.py
+++ searching/MissingNumber.py
@@ -6,9 +6,18 @@
 input = [1, 2, 3, 4, 5, 7]
 inp_len = len(input)
 inp_sum = sum(input)
+miss_num = -1
 
 # Method 1 - Calculate the sum of using length of array and substract with each element to get the remaining count
 sum = round((inp_len + 2) * (inp_len + 1) / 2)
 # print(inp_sum)
 # print(sum)
 print("missing number - " + str(sum - inp_sum))
+
+# method 2 - Sequential search, check if number present at the index
+for i in range(inp_len):
+    if input[i] != i + 1:
+        miss_num = i + 1
+        break
+
+print(miss_num)
