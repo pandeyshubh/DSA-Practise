c479540d8e66b28c7e6921e04cefb2e73dab5738
pandeyshubh <shubhankarpandey21@gmail.com>
2022-01-31 08:26:02 +0530

    LeetCode - Skyline Problem fully working solution

--- leetCode_problems/skyline.py
+++ leetCode_problems/skyline.py
@@ -1,14 +1,29 @@
-#Partial working code - SkyLine Code
+"""
+A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance. Given the locations and heights of all the buildings, return the skyline formed by these buildings collectively.
+
+The geometric information of each building is given in the array buildings where buildings[i] = [lefti, righti, heighti]:
+
+    lefti is the x coordinate of the left edge of the ith building.
+    righti is the x coordinate of the right edge of the ith building.
+    heighti is the height of the ith building.
+
+You may assume all buildings are perfect rectangles grounded on an absolutely flat surface at height 0.
+
+The skyline should be represented as a list of "key points" sorted by their x-coordinate in the form [[x1,y1],[x2,y2],...]. Each key point is the left endpoint of some horizontal segment in the skyline except the last point in the list, which always has a y-coordinate 0 and is used to mark the skyline's termination where the rightmost building ends. Any ground between the leftmost and rightmost buildings should be part of the skyline's contour.
+
+Note: There must be no consecutive horizontal lines of equal height in the output skyline. For instance, [...,[2 3],[4 5],[7 5],[11 5],[12 7],...] is not acceptable; the three lines of height 5 should be merged into one in the final output as such: [...,[2 3],[4 5],[12 7],...]
+
+Input: buildings = [[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]
+Output: [[2,10],[3,15],[7,12],[12,0],[15,10],[20,8],[24,0]]
+
+Check for Skyline problem
+"""
+
 class Solution(object):
 
     #buildings = [[2, 9, 10], [3, 7, 15], [5, 12, 12], [15, 20, 10], [19, 24, 8]]
 
     buildings = [[0, 2, 3] , [2, 5 ,3]]
-
-
-    def isValid(current_ht, ans_list):
-
-        ans_list[len(ans_list)] == current_ht
 
     def getSkyline(self, buildings):
         building_list = list()
@@ -17,13 +32,10 @@ class Solution(object):
         answer_list = list()
 
         for (a, b, c) in buildings:
-            #if not building_list.__contains__([a, c]):
             building_list.append([a, -c])
-            #if not building_list.__contains__([b, c]):
             building_list.append([b, c])
 
-        building_list = sorted(building_list, key=lambda x: x[0])
-        print(building_list)
+        building_list.sort(key=lambda x: (x[0], x[1]))
 
         for (l, h) in building_list:
 
@@ -32,32 +44,14 @@ class Solution(object):
                 height_list.sort(reverse=True)
             else:
                 height_list.remove(h)
-                #print("heelo")
 
             if curr_height != height_list[0]:
                 ht = height_list[0]
                 answer_list.append([l, ht])
                 curr_height = ht
 
-           # if b < 0:
-            #    b = abs(b)
-             #   if b not in height_list:
-              #      height_list.append(b)
-               #     height_list.sort(reverse=True)
-
-                #if b == height_list[0] and curr_height != b:
-                 #   answer_list.append([a, b])
-                  #  curr_height = b
-                #b = -b
-            #if b > 0:
-             #   if b in height_list:
-              #      if b == height_list[0] and curr_height != b:
-               #         answer_list.append([a, height_list[0]])
-                #        curr_height = b
-                 #   height_list.remove(b)
-
-
         print(answer_list)
+        #return answer_list
 
     getSkyline(object, buildings)
 
